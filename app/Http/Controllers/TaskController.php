3ab7905a175f662bacbec13a4c850cc5c27fa267
YusufAgriafan <yusuf.agung.2205356@students.um.ac.id>
2025-01-21 00:24:48 +0700

    update

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
+    public function index()
+    {
+        $tasks = Task::all();
+        return response()->json($tasks, 200);
+    }
     public function store(Request $request)
     {
         $validated = $request->validate([
@@ -19,6 +24,37 @@ class TaskController extends Controller
         $task = Task::create($validated);
 
         return response()->json($task, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $task = Task::findOrFail($id);
+
+        $validated = $request->validate([
+            'title' => 'nullable|string',
+            'description' => 'nullable|string',
+            'project_id' => 'nullable|exists:projects,id',
+            'user_id' => 'nullable|exists:users,id',
+        ]);
+
+        $task->update($validated);
+
+        return response()->json($task, 200);
+    }
+
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+
+        return response()->json(null, 204);
+    }
+
+    public function show($id)
+    {
+        $task = Task::findOrFail($id);
+
+        return response()->json($task, 200);
     }
 
 }
